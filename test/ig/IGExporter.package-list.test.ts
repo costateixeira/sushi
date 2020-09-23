53bc66491ed9355eec68dd3fbdf10e4fa14c5d33
Julia Afeltra <30803904+jafeltra@users.noreply.github.com>
2020-09-23 10:54:47 -0400

    Update handling of non-FSH files (#607)
    * Only rename pagecontent files without numeric prefixes in old IG Publisher context
    
    * Only rename BYO resource filesnames in old IG Publisher context
    
    * Do not merge missing ig.ini properties
    
    - NOTE: THIS IS A BREAKING CHANGE
    - When "ig" and "template" properties are missing, log an error, but do not merge any default values. Copy file as is with missing properties.
    
    * Remove references to ig-data and use base of igDataPath instead
    
    * Support new output patterns
    
    - In new configuration, if a config property is provided and a file is present, prefer the file over config
    - In new configuration, don't copy over file that are already present
    - Support continued copying and config property preferency in legacy configuration
    
    * Update test to test new configuration that logs different warnings and does not copy files
    
    * Address review comments
    
    - Fix boolean logic
    - Don't declare type when setting default value
    - Update warning message to encourage use of own index.md file
    - Rename variables
    - Use path.join rather than construct path using path.sep
    
    * Only copy package-list.json in legacy tank configuration
    
    * Update based on review
    
    - Simplify where we load resources from
    - Fix boolean logic
    - Add test to properly check that no provided resources are copied over in new configuration
    
    * Don't add new line breaks in warning message. Update if/else. Use configName variable.

--- test/ig/IGExporter.package-list.test.ts
+++ test/ig/IGExporter.package-list.test.ts
@@ -1,10 +1,12 @@
 import fs, { readJSONSync } from 'fs-extra';
 import path from 'path';
 import temp from 'temp';
+import { cloneDeep } from 'lodash';
 import { IGExporter } from '../../src/ig';
 import { Package } from '../../src/export';
 import { loggerSpy } from '../testhelpers/loggerSpy';
 import { minimalConfig } from '../utils/minimalConfig';
+import { Configuration } from '../../src/fshtypes';
 
 describe('IGExporter', () => {
   // Track temp files/folders for cleanup
@@ -12,17 +14,20 @@ describe('IGExporter', () => {
 
   describe('#package-list', () => {
     let tempOut: string;
+    let config: Configuration;
 
-    beforeAll(() => {
+    beforeEach(() => {
       tempOut = temp.mkdirSync('sushi-test');
+      config = cloneDeep(minimalConfig);
+      loggerSpy.reset();
     });
 
-    afterAll(() => {
+    afterEach(() => {
       temp.cleanupSync();
     });
 
-    it('should copy package-list.json when ig-data/package-list.json is defined', () => {
-      const pkg = new Package(minimalConfig);
+    it('should copy package-list.json when ig-data/package-list.json is defined in legacy configuration', () => {
+      const pkg = new Package(config);
       const igDataPath = path.resolve(__dirname, 'fixtures', 'customized-ig', 'ig-data');
       const exporter = new IGExporter(pkg, null, igDataPath);
       exporter.addPackageList(tempOut);
@@ -35,7 +40,18 @@ describe('IGExporter', () => {
       expect(outputLogDetails.action).toBe('copied');
       expect(outputLogDetails.inputs).toHaveLength(1);
       expect(outputLogDetails.inputs[0].endsWith('package-list.json')).toBeTruthy();
-      expect(loggerSpy.getLastMessage('info')).toBe('Copied ig-data/package-list.json.');
+      expect(loggerSpy.getLastMessage('info')).toBe(`Copied ig-data${path.sep}package-list.json.`);
+    });
+
+    it('should not copy package-list.json even if ig-data/package-list.json is defined', () => {
+      const pkg = new Package(config);
+      const igDataPath = path.resolve(__dirname, 'fixtures', 'customized-ig', 'ig-data');
+      const exporter = new IGExporter(pkg, null, igDataPath, true); // New tank configuration input/fsh/
+      exporter.addPackageList(tempOut);
+      const pkgListPath = path.join(tempOut, 'package-list.json');
+      expect(fs.existsSync(pkgListPath)).toBeFalsy(); // Do not copy user provided file or generate a new file
+      expect(exporter.getOutputLogDetails(pkgListPath)).toBeUndefined();
+      expect(loggerSpy.getAllMessages('info')).toHaveLength(0);
     });
   });
 });
